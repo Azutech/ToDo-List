9c1d4b6c93b8b27f099dd464d557fca873192f1d
Azutech <azunna.onugha@gmail.com>
2024-04-25 09:49:26 +0100

    Feature() : All request handled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,27 @@
-
+import { useEffect, useState } from "react";
+import Todos from "./components/toDo";
+import { getAllTodo } from "./utils/HandleApp";
 
 function App() {
+  const [toDo, setToDo] = useState([]); // Initialize the state
+
+  // Call the API when the component mounts
+  useEffect(() => {
+    getAllTodo(setToDo); // Pass the state updater function to the API call
+  }, []); // Empty dependency array to run only once on component mount
+
   return (
     <div className="App">
-      <div className = "container">
-        <h1> ToDO App</h1>
-        <div className = 'top'>
-          <input type ='text' placeholder ="Add Todos"/>
-
-          <div className = 'add'>Add</div>
+      <div className="container">
+        <h1>ToDo App</h1>
+        <div className="top">
+          <input type="text" placeholder="Add Todos" />
+          <div className="add">Add</div>
+        </div>
+        <div className="list">
+          {toDo.map((item) => (
+            <Todos key={item._id} text={item.name} />
+          ))}
         </div>
       </div>
     </div>
@@ -16,3 +29,4 @@ function App() {
 }
 
 export default App;
+
